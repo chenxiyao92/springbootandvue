9daae36ee407f469a268005394c192d7cd62f07a
陈希尧 <xychen@zhibodata.cn>
2020-04-29 23:09:09 +0800

    使用https配置 以及开启额外http端口监听并且转发
    使用jdk生成key
    keytool -genkey  -alias tomcathttps -keyalg RSA -validity 365 -keystore D:\upload\sang.p12 -keysize 2048

---
+++ src/main/java/top/cxyhome/springbootandvue/config/TomcatConfig.java
@@ -0,0 +1,33 @@
+package top.cxyhome.springbootandvue.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置一个tomcat监听器 监听8080端口并将接口请求转到8081端口
+ */
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    TomcatServletWebServerFactory tomcatServletWebServerFactory(){
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createTomcatConnect());
+        return tomcatServletWebServerFactory;
+    }
+
+    private Connector createTomcatConnect(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(8080);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(8081);
+        return connector;
+
+    }
+}
---
+++ src/main/java/top/cxyhome/springbootandvue/controller/DemoController.java
@@ -0,0 +1,13 @@
+package top.cxyhome.springbootandvue.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping(value = "/demo")
+    public String demo (){
+        return "demo";
+    }
+}
