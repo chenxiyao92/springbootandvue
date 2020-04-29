f5640f8e1764c7cc467fba5bd952ee1ab902ed0e
陈希尧 <xychen@zhibodata.cn>
2020-04-29 22:12:22 +0800

    2.3定制banner

--- src/main/java/top/cxyhome/springbootandvue/SpringbootandvueApplication.java
+++ src/main/java/top/cxyhome/springbootandvue/SpringbootandvueApplication.java
@@ -1,13 +1,17 @@
 package top.cxyhome.springbootandvue;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootandvueApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootandvueApplication.class, args);
+//        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(SpringbootandvueApplication.class);
+//        springApplicationBuilder.bannerMode(Banner.Mode.OFF).run(args);
     }
 
 }
