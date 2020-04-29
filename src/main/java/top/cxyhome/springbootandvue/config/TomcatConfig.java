5e2b9540091eaf0958c7a8e6fb9d099f8969ab51
陈希尧 <xychen@zhibodata.cn>
2020-04-29 23:15:25 +0800

    不使用tomcat 使用jetty

--- src/main/java/top/cxyhome/springbootandvue/config/TomcatConfig.java
+++ src/main/java/top/cxyhome/springbootandvue/config/TomcatConfig.java
@@ -1,9 +1,9 @@
 package top.cxyhome.springbootandvue.config;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+//import org.apache.catalina.connector.Connector;
+//import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,22 +12,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfig {
 
-    @Bean
-    TomcatServletWebServerFactory tomcatServletWebServerFactory(){
-        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createTomcatConnect());
-        return tomcatServletWebServerFactory;
-    }
-
-    private Connector createTomcatConnect(){
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        //Connector监听的http的端口号
-        connector.setPort(8080);
-        connector.setSecure(false);
-        //监听到http的端口号后转向到的https的端口号
-        connector.setRedirectPort(8081);
-        return connector;
-
-    }
+//    @Bean
+//    TomcatServletWebServerFactory tomcatServletWebServerFactory(){
+//        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+//        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createTomcatConnect());
+//        return tomcatServletWebServerFactory;
+//    }
+//
+//    private Connector createTomcatConnect(){
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        //Connector监听的http的端口号
+//        connector.setPort(8080);
+//        connector.setSecure(false);
+//        //监听到http的端口号后转向到的https的端口号
+//        connector.setRedirectPort(8081);
+//        return connector;
+//
+//    }
 }
