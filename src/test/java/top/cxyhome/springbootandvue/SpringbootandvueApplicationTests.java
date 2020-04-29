04fa79e8baa07e5c3d46a1623192e3b64a3d7a88
陈希尧 <xychen@zhibodata.cn>
2020-04-29 22:07:21 +0800

    Initial commit

---
+++ src/test/java/top/cxyhome/springbootandvue/SpringbootandvueApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cxyhome.springbootandvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootandvueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
